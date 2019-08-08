3119838d46b3c7e6d7e6150d82917d1dffd257f4
JGodoy <j.godoy277@gmail.com>
2019-08-08 17:48:18 -0300

    remove unused imports

--- client/src/main/java/org/evosuite/coverage/epa/EPAFactory.java
+++ client/src/main/java/org/evosuite/coverage/epa/EPAFactory.java
@@ -1,7 +1,6 @@
 package org.evosuite.coverage.epa;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
